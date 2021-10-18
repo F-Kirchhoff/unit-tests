e1abde9d8bdf336a57e576034614b40b7068b867
Felix Kirchhoff <mailfk@phispace.eu>
2021-10-18 14:10:33 +0200

    add first test

---
+++ getGreeting.spec.js
@@ -0,0 +1,17 @@
+import { describe, it } from '@jest/globals'
+import { getGreeting } from './getGreeting.js'
+
+const names = ['Leonardo', 'name', '@tribal', 'something dumb']
+
+describe('getGreeting', () => {
+  it('returns a string "hi stranger!" if called without arguments', () => {
+    const result = getGreeting()
+    expect(result).toBe('hi stranger!')
+  })
+  names.forEach(name => {
+    it(`returns a string "hi ${name}!" if called with input ${name}`, () => {
+      const result = getGreeting(`${name}`)
+      expect(result).toBe(`hi ${name}!`)
+    })
+  })
+})
