8ede64154f74ad4eb01947f7da0becaf71399b92
Felix Kirchhoff <mailfk@phispace.eu>
2021-10-18 11:42:16 +0200

    add replaceName

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ console.log('hi node.')
 function getGreeting(name = 'stranger') {
   return `hi ${name}!`
 }
+
+function replaceName(text, name, newName) {
+  return text.replaceAll(name, newName)
+}
+
+const old = getGreeting('john')
+console.log(old)
+const neww = replaceName(old, 'john', 'markus')
+
+console.log(neww)
